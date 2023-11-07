9223e1feb1789600e8d33598c6cb838241df82d8
aloeJun <69854983+aloeJun@users.noreply.github.com>
2023-11-07 10:16:14 +0800

    修改导航栏信息

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,18 +11,14 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  siteUrl: 'https://sport.islu.cn',
+  logo: 'https://islu.cn/media/detail/aloeJun.png',
   description: 'Personal site and blog',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
-    {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
-    },
+      url: 'https://islu.cn',
+    }
   ],
 };
 
